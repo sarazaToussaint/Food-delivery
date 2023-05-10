240f2ebbc9ec64c4e75229c02389aaf476341618
sarazaToussaint <tmsaraza@gmail.com>
2023-05-10 20:27:29 +0200

    Install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Header } from './components';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="w-screen h-auto flex flex-col">
+      <Header />
+    </div>
   );
 }
 
