040895292bda290b04ba3a3374a20699bfe6f18a
sarazaToussaint <tmsaraza@gmail.com>
2023-05-17 22:48:34 +0200

    Make the bg coloe slate-500 on small screen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header } from './components';
 
 function App() {
   return (
-    <div className="w-screen text-center h-auto flex flex-col">
+    <div className="w-screen text-center sm:bg-slate-500 h-auto flex flex-col">
       <Header />
     </div>
   );
