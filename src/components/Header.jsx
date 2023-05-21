df6b1137781458a7567a7b6420c2e646dab7de8a
sarazaToussaint <tmsaraza@gmail.com>
2023-05-21 11:16:51 +0200

    Delete blank spaces in Header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,19 +18,19 @@ const Header = () => (
         </li>
         <li
           className="text-base text-textColor hover:text-headingColor
-        cursor-pointer duration-100 transition-all ease-in-out"
+          cursor-pointer duration-100 transition-all ease-in-out"
         >
           Menu
         </li>
         <li
           className="text-base text-textColor hover:text-headingColor
-         cursor-pointer duration-100 transition-all ease-in-out"
+          cursor-pointer duration-100 transition-all ease-in-out"
         >
           About Us
         </li>
         <li
           className="text-base text-textColor hover:text-headingColor
-         cursor-pointer duration-100 transition-all ease-in-out"
+          cursor-pointer duration-100 transition-all ease-in-out"
         >
           Services
         </li>
