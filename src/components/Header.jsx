768b72c2107aa4e64ade6c098de0a24664ad394a
sarazaToussaint <tmsaraza@gmail.com>
2023-05-22 23:06:40 +0200

    Make change in Header.js file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => (
 
       <div className="relative flex items-center justify-center">
         <MdShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
-        <div className="absolute top-2 right-2 w-5 h-5 rounded-full bg-red-500
+        <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-red-500
         flex items-center justify-center"
         >
           <p className="text-xs text-white font-semibold">3</p>
