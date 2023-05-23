3b7b818aea2eb55616f1056e492c749e4401ae2c
sarazaToussaint <tmsaraza@gmail.com>
2023-05-23 23:14:49 +0200

    Import CreateContainer file

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as Header } from './Header';
+export { default as MainContainer } from './MainContainer';
+export { default as CreateContainer } from './CreateContainer';
