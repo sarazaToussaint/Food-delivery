2d6a5e6200b34f68eb133eaec2c3448cc29026a0
sarazaToussaint <tmsaraza@gmail.com>
2023-05-24 21:16:08 +0200

    Add login with google functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { MdShoppingBasket } from 'react-icons/md';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { app } from '../firebase.config';
 import Avatar from '../img/avatar.png';
 import Logo from '../img/logo.png';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  const [{ user }, dispatch] = useStateValue();
+  const [isMenu, setIsMenu] = useState(false);
+
   const login = async () => {
-    const response = await signInWithPopup(firebaseAuth, provider);
-    console.log(response);
+    if (!user) {
+      const { user: { providerData } } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem('user', JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
+    }
   };
 
   return (
@@ -63,12 +77,47 @@ const Header = () => {
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
-            src={Avatar}
+            src={user ? user.photoURL : Avatar}
             className="w-10 min-w-[30px] h-10 min-h-[30px]
-        drop-shadow-xl ml-8 cursor-pointer"
+            drop-shadow-xl ml-8 cursor-pointer rounded-full"
             alt="AvatarImage"
             onClick={login}
           />
+          {
+            isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg
+              flex flex-col absolute top-12 right-0"
+              >
+                {
+             user && user.email === 'tmsaraza@gmail.com' && (
+             <Link to="/createItem">
+               <p className="px-4 py-2 flex items-center gap-3
+              cursor-pointer hover:bg-slate-100 transition-all
+               duration-100 ease-in-out text-textColor text-base"
+               >
+                 New Item
+                 {' '}
+                 <MdAdd />
+                 {' '}
+               </p>
+             </Link>
+             )
+           }
+                <p className="px-4 py-2 flex items-center gap-3
+             cursor-pointer hover:bg-slate-100 transition-all
+              duration-100 ease-in-out text-textColor text-base"
+                >
+                  Logout
+                  {' '}
+                  <MdLogout />
+                </p>
+              </motion.div>
+            )
+          }
         </div>
       </div>
 
