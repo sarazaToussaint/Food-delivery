3361ab08bd64672ba95b1b2c3ecd269ffa3654fc
sarazaToussaint <tmsaraza@gmail.com>
2023-05-25 21:50:16 +0200

    Make a change in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Header, MainContainer, CreateContainer } from './components';
 function App() {
   return (
     <AnimatePresence>
-      <div className="w-screen h-auto flex flex-row bg-primary">
+      <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
 
         <main className="mt-24 p-8 w-full">
