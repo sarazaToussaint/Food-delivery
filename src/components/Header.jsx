e44c56fd95b74d7e10eb5ff3c3bc841b33bda7f9
sarazaToussaint <tmsaraza@gmail.com>
2023-05-25 21:54:51 +0200

    Remove unused codes in Header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,16 +29,6 @@ const Header = () => {
     }
   };
 
-  // const logout = () => {
-  //   setIsMenu(false);
-  //   localStorage.clear();
-
-  //   dispatch({
-  //     type: actionType.SET_USER,
-  //     user: null,
-  //   });
-  // };
-
   return (
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
       <div className="hidden md:flex w-full h-full items-center justify-between">
