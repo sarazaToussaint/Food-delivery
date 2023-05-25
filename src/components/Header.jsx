c96d1521ddfc23b56074d31ecdff95cc48e62e19
sarazaToussaint <tmsaraza@gmail.com>
2023-05-25 21:55:21 +0200

    Remove unused codes in Header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
+    <header className="fixed w-screen p-3 px-4 md:p-6 md:px-16">
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to="/" className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
