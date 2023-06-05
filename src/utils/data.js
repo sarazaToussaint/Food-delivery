610900ff9edbee68f0cc8375eb92eab6684ebd09
sarazaToussaint <tmsaraza@gmail.com>
2023-06-05 12:30:27 +0200

    Create an arry of objects in data.js file

---
+++ src/utils/data.js
@@ -0,0 +1,37 @@
+import I1 from '../img/i1.png';
+import F1 from '../img/f1.png';
+import C3 from '../img/c3.png';
+import Fi1 from '../img/fi1.png';
+
+const heroData = [
+  {
+    id: 1,
+    name: 'Icecream',
+    decp: 'Chocolate & Vanilla',
+    price: '5.25',
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: 'Strawberries',
+    decp: 'Fresh Strawberries',
+    price: '10.25',
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: 'Chicken kebab',
+    decp: 'Mixed kebab plat',
+    price: '8.25',
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: 'Fish kebab',
+    decp: 'Fish kebab plat',
+    price: '5.25',
+    imageSrc: Fi1,
+  },
+];
+
+export default heroData;
